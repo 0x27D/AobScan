e5e8ca34412b05cb4be797cb9342ee97a18853dc
0xTheX <huynguyenit2k6@gmail.com>
2023-08-30 15:00:24 +0700

    first commit

---
+++ AobScanExample/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AobScanExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.procname = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.hexscan = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hexrep = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.PID = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // procname
+            // 
+            this.procname.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.procname.DefaultText = "";
+            this.procname.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.procname.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.procname.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.procname.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.procname.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.procname.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.procname.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.procname.Location = new System.Drawing.Point(98, 39);
+            this.procname.Name = "procname";
+            this.procname.PasswordChar = '\0';
+            this.procname.PlaceholderText = "";
+            this.procname.SelectedText = "";
+            this.procname.Size = new System.Drawing.Size(200, 25);
+            this.procname.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Process Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hex Scan";
+            // 
+            // hexscan
+            // 
+            this.hexscan.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.hexscan.DefaultText = "";
+            this.hexscan.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.hexscan.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.hexscan.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.hexscan.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.hexscan.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.hexscan.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.hexscan.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.hexscan.Location = new System.Drawing.Point(98, 82);
+            this.hexscan.Name = "hexscan";
+            this.hexscan.PasswordChar = '\0';
+            this.hexscan.PlaceholderText = "";
+            this.hexscan.SelectedText = "";
+            this.hexscan.Size = new System.Drawing.Size(200, 25);
+            this.hexscan.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hex Replace";
+            // 
+            // hexrep
+            // 
+            this.hexrep.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.hexrep.DefaultText = "";
+            this.hexrep.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.hexrep.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.hexrep.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.hexrep.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.hexrep.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.hexrep.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.hexrep.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.hexrep.Location = new System.Drawing.Point(98, 125);
+            this.hexrep.Name = "hexrep";
+            this.hexrep.PasswordChar = '\0';
+            this.hexrep.PlaceholderText = "";
+            this.hexrep.SelectedText = "";
+            this.hexrep.Size = new System.Drawing.Size(200, 25);
+            this.hexrep.TabIndex = 4;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.LightGray;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(98, 165);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(200, 29);
+            this.guna2Button1.TabIndex = 6;
+            this.guna2Button1.Text = "Start";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // PID
+            // 
+            this.PID.AutoSize = true;
+            this.PID.Location = new System.Drawing.Point(18, 177);
+            this.PID.Name = "PID";
+            this.PID.Size = new System.Drawing.Size(52, 13);
+            this.PID.TabIndex = 7;
+            this.PID.Text = "PID Load";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 219);
+            this.Controls.Add(this.PID);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.hexrep);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hexscan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.procname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2TextBox procname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox hexscan;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox hexrep;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.Label PID;
+    }
+}
+
---
+++ AobScanExample/Form1.cs
@@ -0,0 +1,142 @@
+﻿using Memory;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace AobScanExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct ProcessEntry32
+        {
+            public uint dwSize;
+
+            public uint cntUsage;
+
+            public uint th32ProcessID;
+
+            public IntPtr th32DefaultHeapID;
+
+            public uint th32ModuleID;
+
+            public uint cntThreads;
+
+            public uint th32ParentProcessID;
+
+            public int pcPriClassBase;
+
+            public uint dwFlags;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        }
+        private string sr;
+
+        private int x;
+
+        public Mem MemLib = new Mem();
+
+        private static string string_0;
+        private IContainer icontainer_0;
+        [DllImport("KERNEL32.DLL")]
+        public static extern IntPtr CreateToolhelp32Snapshot(uint flags, uint processid);
+
+        [DllImport("KERNEL32.DLL")]
+        public static extern int Process32First(IntPtr handle, ref ProcessEntry32 pe);
+
+        [DllImport("KERNEL32.DLL")]
+        public static extern int Process32Next(IntPtr handle, ref ProcessEntry32 pe);
+        private async Task PutTaskDelay(int Time)
+        {
+            await Task.Delay(Time);
+        }
+
+        public async void bimsocool(string type)
+        {
+            x = 0;
+            await DzVl(type);
+        }
+        public async void memory(string search, string replace)
+        {
+
+            if (Convert.ToInt32(PID.Text) == 0)
+            {
+                MessageBox.Show("Error");
+            }
+            MemLib.OpenProcess(Convert.ToInt32(PID.Text));
+
+            IEnumerable<long> source = await MemLib.AoBScan(0L, 140737488355327L, (search), writable: true, executable: true);
+            if (source.Count() != 0)
+            {
+                for (int i = 0; i < source.Count(); i++)
+                {
+                    MemLib.WriteMemory(source.ElementAt(i).ToString("X"), "bytes", (replace));
+                }
+
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            MemLib.CloseProcess();
+
+        }
+
+        private async Task<IntPtr> DzVl(string type)
+        {
+            string ProcScan = procname.Text;
+            IntPtr intPtr = IntPtr.Zero;
+            uint num = 0u;
+            IntPtr intPtr2 = CreateToolhelp32Snapshot(2u, 0u);
+            if ((int)intPtr2 > 0)
+            {
+                ProcessEntry32 processEntry = default(ProcessEntry32);
+                processEntry.dwSize = (uint)Marshal.SizeOf(processEntry);
+                for (int num2 = Process32First(intPtr2, ref processEntry); num2 == 1; num2 = Process32Next(intPtr2, ref processEntry))
+                {
+                    IntPtr intPtr3 = Marshal.AllocHGlobal((int)processEntry.dwSize);
+                    Marshal.StructureToPtr(processEntry, intPtr3, fDeleteOld: true);
+                    ProcessEntry32 processEntry2 = (ProcessEntry32)Marshal.PtrToStructure(intPtr3, typeof(ProcessEntry32));
+                    Marshal.FreeHGlobal(intPtr3);
+                    if (processEntry2.szExeFile.Contains(ProcScan) && processEntry2.cntThreads > num)
+                    {
+                        num = processEntry2.cntThreads;
+                        intPtr = (IntPtr)processEntry2.th32ProcessID;
+                    }
+                    
+                    
+                }
+                PID.Text = Convert.ToString(intPtr);
+                await PutTaskDelay(1000);              
+
+                if(type == "Start")
+                {
+                    memory(hexscan.Text, hexrep.Text);
+                }
+
+            }
+            return intPtr;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            bimsocool("Start");
+        }
+    }
+}
